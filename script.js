1a2f0f36770b79001c79ecb6c7e7e6ca85a75161
ronaldof16 <ronaldof16@hotmail.com>
2023-10-13 20:18:45 -0300

    criação do javascript para o botão darkmode / lightmode

--- script.js
+++ script.js
@@ -11,12 +11,30 @@ function adicionarOperador(operador) {
 
 function calcular() {
     let resultado = eval(document.querySelector(".resultado").value);
+
     document.querySelector(".resultado").value = resultado;
 }
 
 function limpar() {
     document.querySelector(".resultado").value = "";
 } 
+
+/* Criação do darkmode e lightmode */
+
+let botaoLight = document.querySelector("button");
+let body = document.body;
+let principal = document.querySelector(".principal")
+let btn = document.querySelectorAll(".btn")
+
+function light() {
+    body.classList.toggle("body-light");
+    principal.classList.toggle("principal-light");
+    btn.forEach(item => {
+        item.classList.toggle("btn-light");
+    })
+}
+
+botaoLight.addEventListener("click", light);
 
 
 
